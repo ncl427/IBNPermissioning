91497eeb54171ead1fd7a37c13b296b92ba583e2
NCL <34230456+ncl427@users.noreply.github.com>
2022-10-17 14:54:29 +0900

    New OTT NFT contract

--- contracts/ottNFT.sol
+++ contracts/ottNFT.sol
@@ -512,6 +512,81 @@ abstract contract Context {
     }
 }
 
+// File @openzeppelin/contracts/access/Ownable.sol@v4.3.1
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _setOwner(_msgSender());
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _setOwner(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(
+            newOwner != address(0),
+            "Ownable: new owner is the zero address"
+        );
+        _setOwner(newOwner);
+    }
+
+    function _setOwner(address newOwner) private {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
 // File @openzeppelin/contracts/utils/Strings.sol@v4.3.1
 
 pragma solidity ^0.8.0;
@@ -625,7 +700,7 @@ pragma solidity ^0.8.0;
  * the Metadata extension, but not including the Enumerable extension, which is available separately as
  * {ERC721Enumerable}.
  */
-contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
+contract ERC721 is Context, Ownable, ERC165, IERC721, IERC721Metadata {
     using Address for address;
     using Strings for uint256;
 
@@ -818,7 +893,7 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
         address from,
         address to,
         uint256 tokenId
-    ) public virtual override {
+    ) public virtual override onlyOwner() {
         //solhint-disable-next-line max-line-length
         require(
             _isApprovedOrOwner(_msgSender(), tokenId),
@@ -835,7 +910,7 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
         address from,
         address to,
         uint256 tokenId
-    ) public virtual override {
+    ) public virtual override onlyOwner() {
         safeTransferFrom(from, to, tokenId, "");
     }
 
@@ -847,7 +922,7 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
         address to,
         uint256 tokenId,
         bytes memory _data
-    ) public virtual override {
+    ) public virtual override onlyOwner() {
         require(
             _isApprovedOrOwner(_msgSender(), tokenId),
             "ERC721: transfer caller is not owner nor approved"
@@ -1430,98 +1505,25 @@ abstract contract ERC721Burnable is Context, ERC721 {
     }
 }
 
-// File @openzeppelin/contracts/access/Ownable.sol@v4.3.1
 
-pragma solidity ^0.8.0;
-
-/**
- * @dev Contract module which provides a basic access control mechanism, where
- * there is an account (an owner) that can be granted exclusive access to
- * specific functions.
- *
- * By default, the owner account will be the one that deploys the contract. This
- * can later be changed with {transferOwnership}.
- *
- * This module is used through inheritance. It will make available the modifier
- * `onlyOwner`, which can be applied to your functions to restrict their use to
- * the owner.
- */
-abstract contract Ownable is Context {
-    address private _owner;
-
-    event OwnershipTransferred(
-        address indexed previousOwner,
-        address indexed newOwner
-    );
-
-    /**
-     * @dev Initializes the contract setting the deployer as the initial owner.
-     */
-    constructor() {
-        _setOwner(_msgSender());
-    }
-
-    /**
-     * @dev Returns the address of the current owner.
-     */
-    function owner() public view virtual returns (address) {
-        return _owner;
-    }
-
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        require(owner() == _msgSender(), "Ownable: caller is not the owner");
-        _;
-    }
-
-    /**
-     * @dev Leaves the contract without owner. It will not be possible to call
-     * `onlyOwner` functions anymore. Can only be called by the current owner.
-     *
-     * NOTE: Renouncing ownership will leave the contract without an owner,
-     * thereby removing any functionality that is only available to the owner.
-     */
-    function renounceOwnership() public virtual onlyOwner {
-        _setOwner(address(0));
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
-     */
-    function transferOwnership(address newOwner) public virtual onlyOwner {
-        require(
-            newOwner != address(0),
-            "Ownable: new owner is the zero address"
-        );
-        _setOwner(newOwner);
-    }
-
-    function _setOwner(address newOwner) private {
-        address oldOwner = _owner;
-        _owner = newOwner;
-        emit OwnershipTransferred(oldOwner, newOwner);
-    }
-}
 
 // File contracts/MondoIR.sol
 
 pragma solidity ^0.8.2;
 
 contract ottSmartContract is
+    Ownable,
     ERC721,
     ERC721Enumerable,
     ERC721URIStorage,
-    ERC721Burnable,
-    Ownable
+    ERC721Burnable
 {
     using Strings for uint256;
     uint counter;
 
     struct NftItem{
         uint256 tokenId;
+        string exp;
         bool valid;
     }
 
@@ -1542,10 +1544,11 @@ contract ottSmartContract is
         _baseURIextended = baseURI_;
     }
 
-    function _createNftItem(uint tokenId) private{
+    function _createNftItem(uint256 tokenId, string memory exp) private{
     // depends on how you wanna store the nft
     _idToNftItem[tokenId]=NftItem(
       tokenId,
+      exp,
       true
     );
      }
@@ -1596,18 +1599,19 @@ contract ottSmartContract is
     function mint(
         address _to,
         uint256 _tokenId,
+        string memory _exp,
         string memory tokenURI_
     ) external onlyOwner {
         counter++;
         _tokenId = counter;
         _mint(_to, _tokenId);
         _setTokenURI(_tokenId, tokenURI_);
-        _createNftItem(_tokenId);
+        _createNftItem(_tokenId, _exp);
 
     }
 
     // The following function is for getting the map of owned tokens
-    function getOwnedNfts(address target) public view onlyOwner returns (NftItem[] memory) {
+    function getOwnedNfts(address target) public view returns (NftItem[] memory) {
         uint256 ownedItemsCount = ERC721.balanceOf(target);
         NftItem[] memory items = new NftItem[](ownedItemsCount);
         for (uint256 i = 0; i < ownedItemsCount; i++) {
